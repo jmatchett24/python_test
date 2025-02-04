9e1de97f3d1ee6f1a685cfd41ed1035ee5f090c0
jmatchett24 <joelmatchett@gmail.com>
2025-02-04 17:08:41 -0500

    Second commit

---
+++ Test.py
@@ -0,0 +1,9 @@
+def in_autotests_we_trust(a, Kobe):
+    if a == Kobe:
+        print('Test passed')
+    else:
+        print('Test failed')
+
+in_autotests_we_trust(10, '10')
+
+in_autotests_we_trust(0, False)
